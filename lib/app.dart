8d06388a121ea3128635b137f44ce0659d82b506
Brendan Ejike <ejike.br@gmail.com>
2021-03-17 01:28:46 +0100

    Setup Project - Add reusable components, utils & helpers. - Refactor codebase

---
+++ lib/app.dart
@@ -0,0 +1,44 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart' hide Router;
+import 'package:flutter/material.dart' hide Router;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_portal/flutter_portal.dart';
+import 'package:navigation_history_observer/navigation_history_observer.dart';
+import 'package:pexza/manager/locator/locator.dart';
+import 'package:pexza/manager/router/export.dart';
+import 'package:pexza/manager/theme/theme.dart';
+import 'package:pexza/utils/utils.dart';
+
+class Pexza extends StatelessWidget {
+  /// This is the entry point for Pexza App
+  const Pexza({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Precache dependencies & images
+    Helpers.precache(context);
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(create: (_) => getIt<ThemeCubit>()),
+      ],
+      child: BlocBuilder<ThemeCubit, AppTheme>(
+        builder: (context, app) => Portal(
+          child: MaterialApp(
+            title: AppStrings.appName.capitalizeFirst(),
+            debugShowCheckedModeBanner: false,
+            theme: app?.themeData() ?? AppTheme.light().themeData(),
+            darkTheme: AppTheme.dark().themeData(),
+            // builder: ExtendedNavigator(
+            //   navigatorKey: App.key,
+            //   initialRoute: Routes.splashScreen,
+            //   observers: [NavigationHistoryObserver()],
+            //   router: Router(),
+            //   guards: [AuthGuard()],
+            // ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/manager/router/router.dart
@@ -0,0 +1,9 @@
+import 'package:auto_route/auto_route_annotations.dart';
+// import 'package:smartlets/features/_404.dart';
+
+@AdaptiveAutoRouter(
+  routes: <AutoRoute>[],
+  generateNavigationHelperExtension: true,
+  preferRelativeImports: false,
+)
+class $Router {}
