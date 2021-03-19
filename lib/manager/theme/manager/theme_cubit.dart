dcc0d8fe0c2928b5110530cbd0b53d9493c68f27
Brendan Ejike <ejike.br@gmail.com>
2021-03-19 11:52:40 +0100

    Set cubit to singleton

--- lib/manager/theme/manager/theme_cubit.dart
+++ lib/manager/theme/manager/theme_cubit.dart
@@ -3,7 +3,7 @@ import 'package:injectable/injectable.dart';
 import 'package:pexza/manager/theme/theme.dart';
 import 'package:pexza/utils/strings.dart';
 
-@Injectable()
+@singleton
 class ThemeCubit extends HydratedCubit<AppTheme> {
   static const String _THEME_KEY = "${AppStrings.appName}-style";
 
